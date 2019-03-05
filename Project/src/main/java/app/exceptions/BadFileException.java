49483d609bd977338aeab85c8079a711861dc0d6
Bhavik <bhavik.tailor101@gmail.com>
2019-03-05 02:18:53 -0500

    document

--- Project/src/main/java/app/exceptions/BadFileException.java
+++ Project/src/main/java/app/exceptions/BadFileException.java
@@ -3,6 +3,9 @@ package app.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/***
+ * Exception used to send back a 400 if the file doesn't exist
+ */
 @ResponseStatus(value= HttpStatus.BAD_REQUEST, reason="Bad file request")
 public class BadFileException extends RuntimeException {
 }
