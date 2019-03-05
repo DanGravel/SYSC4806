2a65d44f9c51cdcc6901141f67f1654dc6dcd357
Bhavik Tailor <bhavik.t1996@gmail.com>
2019-03-04 21:30:15 -0500

    #25 Created exception to send back bad request if file isn't attached (#39)
    * #25 Created exception to send back bad request if file isn't attached
    
    * #25 Got rid of unnecessary import
    
    * document

---
+++ Project/src/main/java/app/Utils/StringUtils.java
@@ -0,0 +1,10 @@
+package app.Utils;
+
+/***
+ * Util class for any string related things. Feel free to add on.
+ */
+public class StringUtils {
+    public static boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+}
