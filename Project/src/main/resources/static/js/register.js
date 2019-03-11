185c7e247bf694929d6f330994febe78b9c20eef
DanGravel <dan0gravel@gmail.com>
2019-03-11 12:46:22 -0400

    #54 Checks if username is already taken

---
+++ Project/src/main/resources/static/js/register.js
@@ -0,0 +1,11 @@
+function checkUsername() {
+    $.get("/register/user",  { username: $("#username").val()}, function (data) {
+        if(data.userExists) {
+            $("#username").val("")
+            $("#username").addClass("is-invalid")
+        }
+        else{
+            $("#username").removeClass("is-invalid")
+        }
+    })
+}
