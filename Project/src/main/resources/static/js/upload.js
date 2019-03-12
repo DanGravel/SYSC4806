5f0dd697fb00cf7bea49fa29d87ebabfa57ed6bc
Bhavik <bhavik.tailor101@gmail.com>
2019-03-12 04:39:06 -0400

    Making some changes to upload form to make it pretty

--- Project/src/main/resources/static/js/upload.js
+++ Project/src/main/resources/static/js/upload.js
@@ -4,7 +4,7 @@ function checkFileSize(event) {
     var fileArr = fileElement.files;
 
     if (!fileArr || !fileArr[0]) {
-        alert("No file loaded");
+        $('#alert').removeClass("hidden");
         event.preventDefault();
         return;
     }
@@ -18,3 +18,31 @@ function checkFileSize(event) {
         return;
     }
 }
+
+$(function() {
+
+    // We can attach the `fileselect` event to all file inputs on the page
+    $(document).on('change', ':file', function() {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+    });
+
+    // We can watch for our custom `fileselect` event like this
+    $(document).ready( function() {
+        $(':file').on('fileselect', function(event, numFiles, label) {
+
+            var input = $(this).parents('.input-group').find(':text'),
+                log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+            if( input.length ) {
+                input.val(log);
+            } else {
+                if( log ) alert(log);
+            }
+
+        });
+    });
+
+});
