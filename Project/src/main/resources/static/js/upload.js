8492f84c5a18203b32383b41ebb1b77aae806f28
Bhavik Tailor <bhavik.tailor101@gmail.com>
2019-03-12 18:46:08 -0400

    Got rid of the unnecessary $(document).ready

--- Project/src/main/resources/static/js/upload.js
+++ Project/src/main/resources/static/js/upload.js
@@ -42,19 +42,17 @@ $(function() {
     });
 
     // We can watch for our custom `fileselect` event like this
-    $(document).ready( function() {
-        $(':file').on('fileselect', function(event, numFiles, label) {
+    $(':file').on('fileselect', function(event, numFiles, label) {
 
-            var input = $(this).parents('.input-group').find(':text'),
-                log = numFiles > 1 ? numFiles + ' files selected' : label;
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
 
-            if( input.length ) {
-                input.val(log);
-            } else {
-                if( log ) alert(log);
-            }
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
 
-        });
     });
 
     $('#close').click(function () {
