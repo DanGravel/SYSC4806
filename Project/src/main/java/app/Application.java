bb5c93a463acf43cc2a5521f9af0b0ab16a6fbe5
Taz-Z <tanzim_Zaman@hotmail.com>
2019-03-14 13:35:58 -0400

    Added base refactor and more cases (#89)

--- Project/src/main/java/app/Application.java
+++ Project/src/main/java/app/Application.java
@@ -1,22 +1,12 @@
 package app;
 
-import app.models.Article;
-import app.models.ArticleState;
-import app.models.Role;
-import app.models.User;
+import app.setup.TestDBSetup;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
@@ -27,40 +17,7 @@ public class Application {
     @Bean
     public CommandLineRunner demo(ArticleRepository articleRepository, UserRepository userRepository, InMemoryUserDetailsManager inMemoryUserDetailsManage) {
         return (args) -> {
-            PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-            // save a couple of customers
-            Article article = new Article();
-            User user1 = new User("test1", encoder.encode("password"), Role.SUBMITTER);
-            User user2 = new User("test2", encoder.encode("password"), Role.EDITOR);
-            User user3 = new User("test3", encoder.encode("password"), Role.REVIEWER);
-            User user4 = new User("test4", encoder.encode("password"), Role.REVIEWER);
-
-            article.setFileName("FileTest1.txt");
-            article.setData("TEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEST".getBytes());
-            article.setFileType("text/plain");
-            article.setDate(new Date());
-            article.addAuthorizedUser(user1);
-            article.addReviewer(Role.EDITOR, user3);
-
-            Article article2 = new Article();
-            article2.setFileName("FileTest1.txt");
-            article2.setData("TEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEST".getBytes());
-            article2.setFileType("text/plain");
-            article2.setDate(new Date());
-            article2.addAuthorizedUser(user1);
-
-            userRepository.save(user1);
-            userRepository.save(user2);
-            userRepository.save(user3);
-            userRepository.save(user4);
-
-            inMemoryUserDetailsManage.createUser(org.springframework.security.core.userdetails.User.withUsername(user1.getUsername()).password(user1.getPassword()).roles(user1.getRole().toString()).build());
-            inMemoryUserDetailsManage.createUser(org.springframework.security.core.userdetails.User.withUsername(user2.getUsername()).password(user2.getPassword()).roles(user2.getRole().toString()).build());
-            inMemoryUserDetailsManage.createUser(org.springframework.security.core.userdetails.User.withUsername(user3.getUsername()).password(user3.getPassword()).roles(user3.getRole().toString()).build());
-            inMemoryUserDetailsManage.createUser(org.springframework.security.core.userdetails.User.withUsername(user4.getUsername()).password(user4.getPassword()).roles(user4.getRole().toString()).build());
-
-            articleRepository.save(article);
-            articleRepository.save(article2);
+            TestDBSetup.setupTestDB(userRepository, articleRepository, inMemoryUserDetailsManage);
         };
     }
 }
