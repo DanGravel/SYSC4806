5a5d1f5fec908e01f99efa849359a22dd6925695
Taz-Z <tanzim_zaman@hotmail.com>
2019-03-15 01:11:48 -0400

    Added datepicker, submit still needs work

--- Project/src/main/resources/static/js/editorTableSort.js
+++ Project/src/main/resources/static/js/editorTableSort.js
@@ -1,3 +1,13 @@
 $(document).ready( function () {
+    //The line below gets the little time icon when choosing the date. There are some bugs wrt bootstrap4 and this is the workaround
+    $.fn.datetimepicker.Constructor.Default = $.extend({}, $.fn.datetimepicker.Constructor.Default, { icons: { time: 'fas fa-clock', date: 'fas fa-calendar', up: 'fas fa-arrow-up', down: 'fas fa-arrow-down', previous: 'far fa-chevron-left', next: 'far fa-chevron-right', today: 'far fa-calendar-check-o', clear: 'far fa-trash', close: 'far fa-times' } });
     $('#editor_table').DataTable();
 } );
+
+
+$(function() {
+    $("[id*='datetimepicker']").datetimepicker();
+    $("[id*='datetimepicker']").change(function (e) {
+        $('#date_form').submit();
+    });
+});
