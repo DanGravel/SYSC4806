66cafbfcd9609437c342d6dd018866ccdd5194b0
DanGravel <dan0gravel@gmail.com>
2019-03-18 13:30:45 -0400

    #81 Moved login tests to its own class

---
+++ Project/src/test/java/app/LoginTest.java
@@ -0,0 +1,42 @@
+package app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Tests login
+     * @throws Exception
+     */
+    @Test
+    @WithMockUser(username="user", password = "password", roles={"SUBMITTER"})
+    public void login() throws Exception {
+
+        mockMvc.perform(formLogin().user("user").password("password"))
+                .andExpect(status().isFound());
+    }
+
+    /**
+     * Tests login with user that is not registered
+     * @throws Exception
+     */
+    @Test
+    public void loginFail() throws Exception {
+        mockMvc.perform(formLogin().user("notreal").password("pass")).andExpect(status().is3xxRedirection());
+    }
+}
