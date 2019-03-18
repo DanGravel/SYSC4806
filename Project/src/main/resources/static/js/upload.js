7b5313345ae3ebbc9382f9055d405e1580d3acba
Bhavik Tailor <bhavik.tailor101@gmail.com>
2019-03-18 14:18:32 -0400

    #91 Changed stuff to bootstrap uploader and has desired behaviour now

--- Project/src/main/resources/static/js/upload.js
+++ Project/src/main/resources/static/js/upload.js
@@ -33,26 +33,13 @@ function alertUpload(errorMsg) {
 
 $(function() {
 
-    // We can attach the `fileselect` event to all file inputs on the page
-    $(document).on('change', ':file', function() {
-        var input = $(this),
-            numFiles = input.get(0).files ? input.get(0).files.length : 1,
-            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [numFiles, label]);
-    });
-
-    // We can watch for our custom `fileselect` event like this
-    $(':file').on('fileselect', function(event, numFiles, label) {
-
-        var input = $(this).parents('.input-group').find(':text'),
-            log = numFiles > 1 ? numFiles + ' files selected' : label;
-
-        if( input.length ) {
-            input.val(log);
-        } else {
-            if( log ) alert(log);
+    $('.custom-file-input').on('change', function() {
+        if (!this.files || !this.files[0]) {
+            return;
         }
 
+        let fileName = this.files[0].name;
+        $(this).siblings('.custom-file-label').addClass("selected").html(fileName);
     });
 
     $('#close').click(function () {
