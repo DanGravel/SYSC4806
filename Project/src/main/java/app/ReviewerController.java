f9ee219f99e8a002283b536486ee76bcffc66cfd
Taz-Z <tanzim_zaman@hotmail.com>
2019-03-19 17:13:34 -0400

    Removed unsued statements

--- Project/src/main/java/app/ReviewerController.java
+++ Project/src/main/java/app/ReviewerController.java
@@ -5,11 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import app.models.Article;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class ReviewerController extends app.Controller {
--- Project/src/test/java/app/ReviewerTest.java
+++ Project/src/test/java/app/ReviewerTest.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
