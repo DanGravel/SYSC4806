95df4cb920e8f8efd6f037f47e3083a9aeb31007
Bhavik <bhavik.tailor101@gmail.com>
2019-03-19 13:56:50 -0400

    Adding some tests

--- Project/src/test/java/app/FileUploadControllerTest.java
+++ Project/src/test/java/app/FileUploadControllerTest.java
@@ -1,11 +1,19 @@
 package app;
 
+import app.models.Article;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,11 +25,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.Response;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,25 +47,32 @@ public class FileUploadControllerTest {
     @Autowired
     private MockMvc mock;
 
-    @Autowired
-    private FileUploadController uploadController;
+    @MockBean
+    private FileUploadController uploadControllerMock;
+
+    @Before
+    public void setup() {
+        Article a = new Article();
+        a.setId(1);
+        a.setData("lksdjhgpiuasdhjg9asorehng;klasjdghas;ldkf0".getBytes());
+        a.setFileName("HEEHEE.txt");
+        a.setFileType("text/plain");
+        a.setDate(new Date());
+    }
 
     @Test
     @WithMockUser(username = "user", password = "password", roles = {"SUBMITTER"})
     public void testGetFileUpload() throws Exception {
         mock.perform(get("/upload"))
                 .andExpect(status().isOk())
-                .andExpect(model().attributeExists("articles"))
-                .andReturn();
-
+                .andExpect(model().attributeExists("articles"));
     }
 
     @Test
     @WithMockUser(username = "user", password = "password", roles = {"REVIEWER"})
     public void testGetFileUpload_NonSubmitter() throws Exception {
         mock.perform(get("/upload"))
-                .andExpect(status().isForbidden())
-                .andReturn();
+                .andExpect(status().isForbidden());
     }
 
     @Test
@@ -62,7 +83,19 @@ public class FileUploadControllerTest {
         mock.perform(MockMvcRequestBuilders.multipart("/upload")
                         .file(mockFile)
                         .with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetFileById() throws Exception {
+        mock.perform(get("/getfile/1"))
                 .andExpect(status().isFound());
     }
 
+    @Test
+    public void deleteFile() throws Exception {
+        mock.perform(delete("/upload/1")
+                        .with(csrf()))
+                .andExpect(status().isFound());
+    }
 }
