d7764db062b21de524f07f663c930a09aedeaa4f
DanGravel <dan0gravel@gmail.com>
2019-03-24 22:45:48 -0400

    #151 Removed depedencies

--- Project/src/test/java/app/BaseTest.java
+++ Project/src/test/java/app/BaseTest.java
@@ -2,10 +2,8 @@ package app;
 
 import app.models.Role;
 import app.models.User;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
